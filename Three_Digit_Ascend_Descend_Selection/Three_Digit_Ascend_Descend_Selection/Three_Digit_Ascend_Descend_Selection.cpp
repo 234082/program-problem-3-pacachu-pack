8aae15f9c611b629c16cc67fc17f358e95556a54
234082 <31855609+234082@users.noreply.github.com>
2017-10-12 10:12:58 -0500

    Edited by Zayam Tariq
    Their descending command was wrong so I changed that, I added comments to make the code easier to read, and I created a loop so that after you entered a number you wouldn't have to

--- Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection.cpp
+++ Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection.cpp
@@ -6,6 +6,8 @@ Assignment Name : Three digit Ascend Descend Selection
 
 Take in 3 digits from the user, seperate those digits and say whether they ascend descend or neither.
 
+Edited by Zayam Tariq
+
 */
 
 // Libraries
@@ -34,41 +36,47 @@ void pause() {
 // MAIN
 
 void main() {
-
-	//define variables:
-	int userNumber;
-	int digit1;
-	int digit2;
-	int digit3;
-
-	//ask for variable
-
-	cout << "Give me a three digit number!" << endl;
-	
-	// take in variable
-
-	cin >> userNumber;
+	int i = 0;
+	for (;i <= 30; i++) {
+		int x;
 
 
-	// seperate number to 3 digits;
+		//define variables:
+		int userNumber;
+		int digit1;
+		int digit2;
+		int digit3;
 
-	digit1 = userNumber / 100; //int division
-	digit2 = userNumber % 100;
-	digit2 = digit2 / 10;
-	digit3 = userNumber % 10;
+		//ask for variable
 
-	// display
-	cout << "The first digit is " << digit1 << " and the second digit is " << digit2 << " and the last digit is " << digit3 << endl;
-	//calculate ascending descending or neither:
-	if (digit1 > digit2 && digit2 > digit1) {
-		cout << "Your number is descending!" << endl;
+		cout << "Give me a three digit number" << endl;
+
+		// take in variable
+
+		cin >> userNumber;
+
+
+		// seperate number to 3 digits;
+
+		digit1 = userNumber / 100; //int division, creates the hundreds-value number
+		digit2 = userNumber % 100;
+		digit2 = digit2 / 10; // creates the tens-value number
+		digit3 = userNumber % 10; // creates the ones-value number
+
+		// display
+		cout << "The first digit is " << digit1 << " and the second digit is " << digit2 << " and the last digit is " << digit3 << endl;
+		//calculate ascending descending or neither:
+		if (digit1 > digit2 && digit2 > digit3) { // the hundreds-value digit is greater than the tens-value digit is greater than the ones-value digit
+			cout << "Your number is descending!" << endl;
+		}
+		else if (digit1 < digit2 && digit2 < digit3) { // the hundreds-value digit is less than the tens-value digit is less than the ones-value digit
+			cout << "Your number is ascending!" << endl;  // It's not the first condition, but it matches the second condition, so you use "else if"
+		}
+		else { // number is not descending, number is not ascending, use "else" to show that it doesn't match the first OR the second condition
+			cout << "Your number is neither ascending or descending." << endl;
+		}
 	}
-	else if (digit1 < digit2 && digit2 < digit3) {
-		cout << "Your number is ascending!" << endl;
-	}
-	else {
-		cout << "Your number is neither ascending or descending." << endl;
-	}
-
 	pause(); // pauses to see the displayed text
 }
+
+
